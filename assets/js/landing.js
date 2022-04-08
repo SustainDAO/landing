b8d533e1d7eb7fc4674e3e9d24af765af0fcdb97
Aram <arammartirosyann16@gmail.com>
2022-04-09 00:14:34 +0400

    close accordion onclick

--- assets/js/landing.js
+++ assets/js/landing.js
@@ -1,44 +1,68 @@
 let button = document.querySelector("#learn-more");
 button.onclick = () => {
   // document.querySelector("#main").scrollIntoView({ behavior: "smooth" });
-  let ios = isIOS()
-  doScrolling('#main', ios ? 1000 : 50)
+  let ios = isIOS();
+  doScrolling("#main", ios ? 1000 : 50);
 };
 
 function isIOS() {
-  return [
-    'iPad Simulator',
-    'iPhone Simulator',
-    'iPod Simulator',
-    'iPad',
-    'iPhone',
-    'iPod'
-  ].includes(navigator.platform)
-  // iPad on iOS 13 detection
-  || (navigator.userAgent.includes("Mac") && "ontouchend" in document)
+  return (
+    [
+      "iPad Simulator",
+      "iPhone Simulator",
+      "iPod Simulator",
+      "iPad",
+      "iPhone",
+      "iPod",
+    ].includes(navigator.platform) ||
+    // iPad on iOS 13 detection
+    (navigator.userAgent.includes("Mac") && "ontouchend" in document)
+  );
 }
 
 function getElementY(query) {
-  return window.pageYOffset + document.querySelector(query).getBoundingClientRect().top
+  return (
+    window.pageYOffset +
+    document.querySelector(query).getBoundingClientRect().top
+  );
 }
 
 function doScrolling(element, duration) {
-	var startingY = window.pageYOffset
-  var elementY = getElementY(element)
-  var targetY = document.body.scrollHeight - elementY < window.innerHeight ? document.body.scrollHeight - window.innerHeight : elementY
-	var diff = targetY - startingY
-  var easing = function (t) { return t<.5 ? 4*t*t*t : (t-1)*(2*t-2)*(2*t-2)+1 }
-  var start
+  var startingY = window.pageYOffset;
+  var elementY = getElementY(element);
+  var targetY =
+    document.body.scrollHeight - elementY < window.innerHeight
+      ? document.body.scrollHeight - window.innerHeight
+      : elementY;
+  var diff = targetY - startingY;
+  var easing = function (t) {
+    return t < 0.5 ? 4 * t * t * t : (t - 1) * (2 * t - 2) * (2 * t - 2) + 1;
+  };
+  var start;
 
-  if (!diff) return
-	window.requestAnimationFrame(function step(timestamp) {
-    if (!start) start = timestamp
-    var time = timestamp - start
-    var percent = Math.min(time / duration, 1)
-    percent = easing(percent)
-    window.scrollTo(0, startingY + diff * percent)
+  if (!diff) return;
+  window.requestAnimationFrame(function step(timestamp) {
+    if (!start) start = timestamp;
+    var time = timestamp - start;
+    var percent = Math.min(time / duration, 1);
+    percent = easing(percent);
+    window.scrollTo(0, startingY + diff * percent);
     if (time < duration) {
-      window.requestAnimationFrame(step)
+      window.requestAnimationFrame(step);
     }
-  })
+  });
+}
+
+document.querySelectorAll("input[type=radio]").forEach((elem) => {
+  elem.onclick = allowUncheck;
+  elem.previous = elem.checked;
+});
+
+function allowUncheck(e) {
+  if (this.previous) {
+    this.checked = false;
+  }
+  document.querySelectorAll("input[type=radio]").forEach((elem) => {
+    elem.previous = elem.checked;
+  });
 }
