f37e0b4d529b8df5498923f8f21fea5dd138c056
aram <arammartirosyann16@gmail.com>
2022-04-27 19:18:47 +0400

    copy article url

--- assets/js/article.js
+++ assets/js/article.js
@@ -1,4 +1,21 @@
-let btn = document.querySelector('.copy-btn')
-btn.click = () => {
-    navigator.clipboard.writeText(window.location.href)
+function copyUrl() {
+    if (!window.getSelection) {
+      alert('Please copy the URL from the location bar.');
+      return;
+    }
+    const dummy = document.createElement('p');
+    dummy.textContent = window.location.href;
+    document.body.appendChild(dummy);
+    const range = document.createRange();
+    range.setStartBefore(dummy);
+    range.setEndAfter(dummy);
+    const selection = window.getSelection();
+    selection.removeAllRanges();
+    selection.addRange(range);
+    document.execCommand('copy');
+    document.body.removeChild(dummy);
+  }
+
+document.querySelector('.copy-btn').onclick = () => {
+    copyUrl();
 }
