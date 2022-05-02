651e22061176f868d8d0ec3b54baa93d04e979ef
aram1l7 <arammartirosyann16@gmail.com>
2022-05-02 14:38:40 +0400

    test animation

---
+++ src/components/Animation.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import Rive from 'rive-react'
+
+const Animation = () => (
+ <div className='text-white'>
+     <Rive src="../../assets/animations/test.riv"/>
+ </div>
+);
+
+
+export default Animation;
