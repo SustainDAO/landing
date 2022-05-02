3b5256f837140bb75595cdec5f5393bb8e71b9fd
aram1l7 <arammartirosyann16@gmail.com>
2022-05-02 13:20:45 +0400

    integrate react

---
+++ src/components/Component1.jsx
@@ -0,0 +1,11 @@
+// @flow
+import React from 'react';
+
+const Component1 = () => (
+ <div className='text-white'>
+  React asdasdasd
+ </div>
+);
+
+
+export default Component1;
---
+++ src/entry.jsx
@@ -0,0 +1,7 @@
+// @flow
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Component1 from './components/Component1';
+import './modules/article'
+import './modules/index'
+ReactDOM.render(<Component1 />, document.getElementById('root'));
