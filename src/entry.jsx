62b0663ea48628ac5becda860f6b5d0136db41e2
aram1l7 <arammartirosyann16@gmail.com>
2022-05-03 17:09:16 +0400

    animation fix

--- src/entry.jsx
+++ src/entry.jsx
@@ -1,7 +1,8 @@
 // @flow
 import React from 'react';
 import ReactDOM from 'react-dom';
-import Animation from './components/Animation';
+import  Animation  from './components/Animation';
 import './modules/article'
 import './modules/index'
+
 ReactDOM.render(<Animation />, document.getElementById('root'));
